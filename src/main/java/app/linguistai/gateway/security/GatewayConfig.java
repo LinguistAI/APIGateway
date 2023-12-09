2928aa4d50759068716c25cdf187a98e45ac0522
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2023-12-09 20:55:11 +0300

    feat(LINGUIST-125): Updated config files to get uris from the environment

--- src/main/java/app/linguistai/gateway/security/GatewayConfig.java
+++ src/main/java/app/linguistai/gateway/security/GatewayConfig.java
@@ -1,6 +1,7 @@
 package app.linguistai.gateway.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,16 +13,21 @@ public class GatewayConfig {
     @Autowired
     JWTFilter filter;
 
+    @Value("${spring.uri.user}")
+    private String URI_USER;
+    
+    @Value("${spring.uri.dictionary}")
+    private String URI_DICTIONARY;
+
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
+                .route("dictionary-service", r -> r.path("/api/v1/dict/**")
+                        .filters(f -> f.filter(filter))
+                        .uri(URI_DICTIONARY))
                 .route("user-service", r -> r.path("/api/v1/**")
                         .filters(f -> f.filter(filter))
-                        .uri("http://localhost:8081"))
-
-                // .route("auth-service", r -> r.path("/auth/**")
-                //         .filters(f -> f.filter(filter))
-                //         .uri("lb://auth-service"))
+                        .uri(URI_USER))                
                 .build();
     }
 
