12978f790b4d69365df7dc47e32d0d920881bdbf
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2024-01-30 22:18:44 +0300

    Added smth

--- src/main/java/app/linguistai/gateway/exception/ExceptionHandler.java
+++ src/main/java/app/linguistai/gateway/exception/ExceptionHandler.java
@@ -21,7 +21,9 @@ public class ExceptionHandler implements ErrorWebExceptionHandler {
     
     @Override
     public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        System.out.println("aloooooo");
         Logger.error(ex.getMessage());
+        ex.printStackTrace();
 
         if (ex instanceof java.net.ConnectException || ex instanceof org.springframework.web.client.ResourceAccessException) {
             return handleMicroserviceNotWorking(exchange, ex);
@@ -30,7 +32,6 @@ public class ExceptionHandler implements ErrorWebExceptionHandler {
         } else if (ex instanceof JWTException) {
             return handleJWTException(exchange, (JWTException) ex);
         } else {
-            ex.printStackTrace();
             return Mono.error(ex);
         }        
     }
